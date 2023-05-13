31fbc3998543464300cf860db3a137a8c765aa95
Ben Zhao <112284084+bcz028@users.noreply.github.com>
2023-05-13 12:19:29 -0400

    Add files via upload

---
+++ wordle.py
@@ -0,0 +1,67 @@
+def wordle(guess,word):#does not check is the guess, or word, is an actual word
+    count=0
+    if len(guess)!=len(word):
+        return "Guess is not the correct length"
+    word_freq={}
+    possible_repeats=[]
+    final_progress=""
+    correct_placement=[]
+    wrong_placement=[]
+    wrong_letter=[]
+    for letter in word:
+        if letter not in word_freq:
+            word_freq[letter]=1
+        else:
+            word_freq[letter]+=1
+    for index in range(len(guess)):
+        if guess[index]==word[index]:
+            word_freq[guess[index]]-=1
+            count+=1
+            final_progress+=str(guess[index]) + " "
+            correct_placement+=[str(guess[index])+ " " + "is in the correct place"]
+        elif guess[index] in word_freq and word_freq[guess[index]]>0:
+            possible_repeats+=[guess[index]]
+            final_progress+="_ "
+            wrong_placement+=[str(guess[index])+ " " + "is in the wrong place"]
+        else:
+            final_progress+="_ "
+            if guess[index] not in guess[:index]:#accounts for repeated letters not in word
+                wrong_letter+=[str(guess[index])+ " " + "is not in the word"]
+    if count==5:
+        return(word + " " + "is correct: You win!")
+    print("Letters in the correct place:")
+    for message in correct_placement:
+        print(message)
+    if len(correct_placement)==0:
+        print("None")
+    print("\nProgress:")
+    print(final_progress)
+    print("\nLetters in the word, but wrong place:")
+    for message in wrong_placement:
+        print(message)
+    if len(wrong_placement)==0:
+        print("None")
+    print("\nThese letters are not in the word:")
+    for message in wrong_letter:
+        print(message)
+    if len(wrong_letter)==0:
+        print("None")
+    return "\nTry Again!"
+    
+#implement new function to track prevoiusly guessed words
+
+###
+###
+#invokation
+guess="truce"   #put new guess in between quotes
+###
+###
+###
+secret_word="heard"     #put new secret word in between quotes
+###
+###
+###
+print(wordle(guess,secret_word))
+
+
+
